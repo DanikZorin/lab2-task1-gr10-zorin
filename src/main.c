318043846695a50b8a7391c948f69996531c2c48
Danik <61231265+DanikZorin@users.noreply.github.com>
2026-03-13 03:09:49 +0000

    content update

--- src/main.c
+++ src/main.c
@@ -2,23 +2,47 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
-
+#include <time.h>
 void task1(int n,int *a){
+        int count = 0;
         for (int i=0;i<n;i++){
                 int sq = sqrt(a[i]);
                 if (sq*sq == a[i]){
+                        if (count == 0)
+                                printf("Perfect squares:\n");
                         printf("%d\n",a[i]);
+                        count++;
                 }
+        }
+        if (count == 0){
+                printf("No perfect squares\n");
         }
 }
 void main (int argc, char *args[]){
-        int n = atoi(args[1]);
+        int n;
+        if (argc == 2){
+                n = atoi(args[1]);
+        }
+        else{
+                n = argc-1;
+        }
+        
         int a[n];
-        for (int i=0;i<n;i++){
-                a[i] = rand()%10;
+        if (argc == 2){
+                srand(time(0));
+                for (int i=0;i<n;i++){
+                        a[i] = rand()%10;
+                }
+        }
+        else{
+                for (int i=1;i<argc;i++){
+                        a[i-1] = atoi(args[i]);
+                }
         }
         printf("Array:\n");
         for (int i=0;i<n;i++){
                 printf("%d ",a[i]);
         }
+        printf("\n");
+        task1(n,a);
 }
